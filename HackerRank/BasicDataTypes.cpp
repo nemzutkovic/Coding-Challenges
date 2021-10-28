a5bff3743edfb36e291f6dceb0d515ab0d796f4e
Nem Zutkovic <nemzutkovic@icloud.com>
2021-10-27 21:56:39 -0400

    Starting up coding challenges again

---
+++ HackerRank/BasicDataTypes.cpp
@@ -0,0 +1,21 @@
+// Basic Data Types
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int integer;
+    long num;
+    char character;
+    float decimal;
+    double complex;
+    cin >> integer >> num >> character >> decimal >> complex;
+    printf("%d \n",integer);
+    printf("%ld \n", num);
+    printf("%c \n", character);
+    printf("%f \n", decimal);
+    printf("%lf \n", complex);
+    return 0;
+}
